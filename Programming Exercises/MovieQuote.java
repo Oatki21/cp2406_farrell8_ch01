512edd4d657ee91313f62e3a31141b85b3bd135e
jc428928 <oliver.atkinson@my.jcu.edu.au>
2017-08-04 10:43:42 +1000

    My Prac 1 Solution

---
+++ Programming Exercises/MovieQuote.java
@@ -0,0 +1,6 @@
+public class MovieQuote {
+/*  This class displays a test message */
+public static void main(String[] args) {
+    System.out.println("For you.");
+}
+}
---
+++ Programming Exercises/MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+public class MovieQuoteInfo {
+
+    public static void main(String[] args){
+        System.out.println("'For you.'");
+        System.out.println("The Dark Knight Rises");
+        System.out.println("Bane");
+        System.out.println("2012");
+    }
+}
